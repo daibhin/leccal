7d0587cf0844f1d6dc3f9d166462b091a3253763
David Newell <d.newell1@outlook.com>
2017-01-29 19:47:08 +0000

    Update gems and Calendar API

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -35,6 +35,7 @@ gem 'rack-cors', require: 'rack/cors'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'pry-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 end
--- app/commands/calendar/calendar_events/get.rb
+++ app/commands/calendar/calendar_events/get.rb
@@ -4,7 +4,6 @@ module Calendar
 
       required do
         integer :course_id
-        integer :lecturer_id
       end
 
       def execute
@@ -13,29 +12,18 @@ module Calendar
         course.students.each do |student|
           student.courses.each do |course|
             course.upcoming_assignments.each do |assignment|
-              assignment_counts[assignment] += 1
+              assignment_counts[assignment.due_date] += 1
             end
           end
         end
 
-        calendar_events = assignment_counts.map do |assignment|
-          CalendarEventSerializer.new(assignment, root: false).as_json
-        end
-
-        { calendar:
-          {
-            id: "#{lecturer.id}#{course.id}",
-            calendar_events: calendar_events
-          }
+        {
+          calendar_events: assignment_counts
         }
       end
 
       def course
         @course ||= Course.find(course_id)
-      end
-
-      def lecturer
-        @lecturer ||= Lecturer.find(lecturer_id)
       end
     end
   end
